2b49c51b058ec836850d4d2613c1a9890ebe8789
Amr Yehia <amr.yehiaz100@gmail.com>
2024-03-09 22:54:38 +0200

    last

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ const Server = socketIo.Server
 const PORT = process.env.PORT || 3500
 const app = express()
 let userName = ""
+const users = {};
+
 app.use(bodyParser.urlencoded({extended: true}))
 app.get('/', (req,res) => res.sendFile(path.join(__dirname,'../dist/index.html')))
 app.get('/api/node_modules/bootstrap/dist/css/bootstrap.min.css', (req,res) => res.sendFile(path.join(__dirname,'../api/node_modules/bootstrap/dist/css/bootstrap.min.css')))
@@ -13,6 +15,7 @@ app.get('/api/node_modules/bootstrap/dist/js/bootstrap.min.js', (req,res) => res
 app.get('/style.css', (req,res) => res.sendFile(path.join(__dirname,'../dist/style.css')))
 app.get('/app.js', (req,res) => res.sendFile(path.join(__dirname,'../dist/app.js')))
 app.post('/chat', (req, res) => {
+    userName = req.body.UserName
     res.sendFile(path.join(__dirname, "../dist/chat.html"));
 })
 
@@ -27,27 +30,30 @@ const io = new Server(expressServer, {
 })
 
 io.on('connection', socket => {
-    console.log(`User ${socket.id} connected`)
+    users[socket.id] = userName;
+    console.log(`User ${users[socket.id]} connected`)
 
     //Upon connection - only to user
     socket.emit('message', "Welcome to Chat App!")
 
     //Upon connection - to all others
-    socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)} connected`)
+    socket.broadcast.emit('message', `User ${users[socket.id]} connected`)
 
     //listening for a message event
     socket.on('message', data => {
         console.log(data)
-        io.emit('message', `${socket.id.substring(0, 5)}: ${data}`)
+        io.emit('message', `${users[socket.id]}: ${data}`)
     })
 
     //when user disconnects - to all others
     socket.on('disconnect', () => {
-        socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)} disconnected`)
+        socket.broadcast.emit('message', `User ${users[socket.id]} disconnected`)
+        delete users[socket.id];
     })
 
     //Listen for activity
     socket.on('activity', (name) => {
+        name = users[socket.id]
         socket.broadcast.emit('activity', name)
     })
 })
